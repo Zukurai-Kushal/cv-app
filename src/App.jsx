e5c930be7cabe474cef4da80d6483a2390ff117a
Kushal <65113523+Zukurai-Kushal@users.noreply.github.com>
2025-02-23 10:28:41 +0800

    Add print feature for Resume component

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import Resume from './components/Resume'
 import { Dwight_Schrute , Kushal, Clear_State} from './components/data'
 import './App.css'
 import './styles/fonts.css'
-import { useState } from 'react'
+import { useState , useRef } from 'react'
+import { useReactToPrint } from "react-to-print";
+
 
 export default function App() {
   const [resumeData, setResumeData] = useState({ ...Dwight_Schrute });
+  const contentRef = useRef(null);
+  const reactToPrintFn = useReactToPrint({ contentRef: contentRef, documentTitle: (resumeData.fullName+" Resume")});
   return <>
     <Header />
     <div id="content">
       <div id="UI">
         <Form resumeData={resumeData} setResumeData={setResumeData}/>
       </div>
-      <Resume {...resumeData} />
-      <button>Print</button>
+      <div ref={contentRef}><Resume {...resumeData} /></div>
+      <button onClick={() => reactToPrintFn()}>Print</button>
     </div>
   </>
 }
